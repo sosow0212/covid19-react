89ce618e867ff7750097a1e26c34c7baf0879b3b
sosow0212 <sosow0212@naver.com>
2021-09-07 19:05:02 +0900

    Final

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import React from 'react'
 // rafce 치면 알아서 기본 코드 제공함
 
 const header = () => {
-    return (
-        <header className="header">
-            <h1>COVID-19</h1>
-            <select>
-                <option>국내</option>
-                <option>세계</option>
-            </select>
-        </header>
-    )
+	return (
+		<header className="header">
+			<h1>COVID-19</h1>
+			<select>
+				<option>국내</option>
+				<option>세계</option>
+			</select>
+		</header>
+	)
 }
 
 export default header
